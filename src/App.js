2363477b1bf79cf2a1b4fe1792d4fad6e967b66d
Sean Hackett <100976172+TropicalXU@users.noreply.github.com>
2022-06-27 14:55:29 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Die from './Die';
+import RollDice from './RollDice';
+import './Die.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+     <RollDice />
+    </div>
+  )
+}
+
+export default App;
